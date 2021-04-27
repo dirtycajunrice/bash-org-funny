2021d5dd1cdfb62dd91317575f429c967608cb1e
Nicholas St. Germain <nick@cajun.pro>
2021-04-27 04:40:24 -0500

    lol

--- main.go
+++ main.go
@@ -7,23 +7,41 @@ import (
 	"math/rand"
 	"net/http"
 	"regexp"
+	"strings"
 	"time"
 )
 
 func quoteGrab(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().Unix())
-	resp, _ := http.Get("http://bash.org/?random1")
-	b, _ := ioutil.ReadAll(resp.Body)
+
+	resp, err := http.Get("http://bash.org/?random1")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	page := html.UnescapeString(string(b))
+	if strings.Contains(page, "Sorry, the MySQL daemon appears to be down.1Error: problem getting page...") {
+		http.Error(w, "We keep breaking bash.org's mysql... eek.", http.StatusGatewayTimeout)
+	}
 	re := regexp.MustCompile(`.*<p class="qt">(.*)</p>.*`)
 	sma := re.FindAllStringSubmatch(page, -1)
+	if len(sma) == 0 {
+		fmt.Printf("Error: problem getting page...\n%s", page)
+		http.Error(w, http.ErrNoLocation.Error(), http.StatusInternalServerError)
+		return
+	}
 	n := rand.Intn(len(sma))
 	_, _ = fmt.Fprint(w, sma[n][1])
 }
-func main()  {
+func main() {
+	rand.Seed(time.Now().Unix())
 	http.HandleFunc("/", quoteGrab)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		return 
+		return
 	}
 }
